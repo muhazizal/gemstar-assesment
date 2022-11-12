74dcadf45b0b2e8f4170643b67de7072a86d1b91
muhazizal <muhazizal.26@gmail.com>
2022-11-12 14:15:08 +0700

    feat(api): add format rupiah

--- src/components/Dashboard/Approval/DashboardApprovalListHeader.jsx
+++ src/components/Dashboard/Approval/DashboardApprovalListHeader.jsx
@@ -2,9 +2,9 @@ export default function DashboardApprovalListHeader() {
 	return (
 		<div className='py-[13px] px-4 bg-white shadow-[2px_2px_4px_rgba(0,0,0,0.05)] rounded-[5px] grid gap-3 grid-cols-12 text-secondary'>
 			<div className='col-span-2'>Image</div>
-			<div className='col-span-4'>Name</div>
+			<div className='col-span-3'>Name</div>
 			<div className='col-span-3'>Current price</div>
-			<div className='col-span-3'>Market cap</div>
+			<div className='col-span-4'>Market cap</div>
 		</div>
 	)
 }
--- src/components/Dashboard/Approval/DashboardApprovalListItem.jsx
+++ src/components/Dashboard/Approval/DashboardApprovalListItem.jsx
@@ -2,16 +2,40 @@ export default function DashboardApprovalListItem(props) {
 	const { image, name, current_price, market_cap } = props.approval
 	const classRow = 'flex flex-auto flex-row items-center'
 
+	const formatNumberToRupiah = (number) => {
+		let rupiah = ''
+		let real = number.toString().split('.')[0]
+		let decimal = number.toString().split('.')[1]
+		real = real.toString().split('').reverse().join('')
+		for (let i = 0; i < real.length; i++) {
+			if (i % 3 === 0) {
+				rupiah += real.substr(i, 3) + '.'
+			}
+		}
+		return (
+			'Rp ' +
+			rupiah
+				.split('', rupiah.length - 1)
+				.reverse()
+				.join('') +
+			(decimal ? `,${decimal}` : '')
+		)
+	}
+
 	return (
 		<div className='py-[13px] px-4 bg-white shadow-[2px_2px_4px_rgba(0,0,0,0.05)] rounded-[5px] grid gap-4 grid-cols-12 text-secondary'>
 			<div className={`col-span-2 ${classRow}`}>
 				<img className='max-w-[25%]' src={image} alt={`${name} Asset`} />
 			</div>
-			<div className={`col-span-4 ${classRow}`}>
+			<div className={`col-span-3 ${classRow}`}>
 				<div className='truncate font-bold'>{name}</div>
 			</div>
-			<div className={`col-span-3 ${classRow}`}>{current_price}</div>
-			<div className={`col-span-3 ${classRow}`}>{market_cap}</div>
+			<div className={`col-span-3 ${classRow}`}>
+				{formatNumberToRupiah(current_price)}
+			</div>
+			<div className={`col-span-4 ${classRow}`}>
+				{formatNumberToRupiah(market_cap)}
+			</div>
 		</div>
 	)
 }
